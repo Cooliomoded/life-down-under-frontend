bf5d97d59ff5e9a007c986348d505f755656993a
noaheakin <noaheakin@gmail.com>
2020-11-13 11:24:08 -0800

    added default radio selection

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,18 +13,18 @@ class SearchBar extends Component {
                 <div className='search-bar'>
                     <h3>Search For Australian Wildlife</h3>
                     <form onSubmit={handleSearchSubmit}>
-                        <input type="radio" id="animal" name="kingdom" value="animals"></input>
-                        <label htmlFor="animal">Animal</label>
-                        <input type="radio" id="plant" name="kingdom" value="plants"></input>
-                        <label htmlFor="plant">Plant</label>
-                        <input type="radio" id="fungi" name="kingdom" value="fungi"></input>
-                        <label htmlFor="fungi">Fungi</label>
-                        <input type="radio" id="bacteria" name="kingdom" value="bacteria"></input>
+                        <input type="radio" id="bacteria" name="kingdom" value="bacteria" checked></input>
                         <label htmlFor="bacteria">Bacteria</label>
-                        <input type="radio" id="protists" name="kingdom" value="protists"></input>
+                        <input type="radio" id="protists" name="kingdom" value="protists" checked></input>
                         <label htmlFor="protists">Protists</label>
-                        <input type="radio" id="protozoans" name="kingdom" value="protozoans"></input>
-                        <label htmlFor="protozoans">Protozoans</label><br></br><br></br>
+                        <input type="radio" id="protozoans" name="kingdom" value="protozoans" checked></input>
+                        <label htmlFor="protozoans">Protozoans</label>
+                        <input type="radio" id="fungi" name="kingdom" value="fungi" checked></input>
+                        <label htmlFor="fungi">Fungi</label>
+                        <input type="radio" id="plant" name="kingdom" value="plants" checked></input>
+                        <label htmlFor="plant">Plant</label>
+                        <input type="radio" id="animal" name="kingdom" value="animals" checked></input>
+                        <label htmlFor="animal">Animal</label><br></br><br></br>
                         <input type="text" id="animal-search" name="animal" placeholder="Search a Species" required></input>
                         <input type="submit" id="animal-search-submit" name="animal-search-submit"></input>
                     </form>
